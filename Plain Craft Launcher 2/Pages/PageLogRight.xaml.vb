5338f0b9775f5fc446d45b3d8383713351a68391
youzi-3222 <youzi_2333@163.com>
2025-06-05 22:58:20 +0800

    docs(realtime-log): 添加 i18n 相关注释

--- Plain Craft Launcher 2/Pages/PageLogRight.xaml.vb
+++ Plain Craft Launcher 2/Pages/PageLogRight.xaml.vb
@@ -1,6 +1,7 @@
 ﻿Public Class PageLogRight
     Public Sub Init() Handles Me.Initialized
         PanLogCard.Inlines.Clear()
+        'TODO(i18n): 文本 @ 标题栏 - 实时日志卡片标题
         PanLogCard.Inlines.Add(New Run("实时日志"))
         PanLogCard.Inlines.Add(New Run(" | "))
         LabDebug = New Run("0 Debug") With {.Foreground = Application.Current.Resources("ColorBrushDebug")}
@@ -107,12 +108,14 @@
     Private Sub BtnOperationExportStackDump_Click(sender As Object, e As RouteEventArgs) Handles BtnOperationExportStackDump.Click
         Dim SavePath As String = SelectSaveFile("选择导出位置", $"游戏运行栈 - {Date.Now.ToString("G").Replace("/", "-").Replace(":", ".").Replace(" ", "_")}.log", "游戏运行栈(*.log)|*.log")
         If SavePath.Length < 3 Then Exit Sub
+        'TODO(i18n): 文本 @ 左下角提示 - 导出运行栈提示
         Hint("正在导出运行栈，请稍等（可能需要 15 秒 ~ 1 分钟）", HintType.Info)
         BtnOperationExportStackDump.IsEnabled = False
         RunInNewThread(Sub()
                            Dim Dump = FrmLogLeft.CurrentLog.ExportStackDump(SavePath)
                            File.WriteAllLines(SavePath, Dump)
                            RunInUi(Sub()
+                                       'TODO(i18n): 文本 @ 左下角提示 - 导出运行栈提示
                                        Hint("运行栈已导出！", HintType.Finish)
                                        BtnOperationExportStackDump.IsEnabled = True
                                    End Sub)
