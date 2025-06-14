8ee90441802d34a9478fe24510e03e5d4ee03b6a
tangge233 <tangge233@qq.com>
2025-06-15 01:27:51 +0800

    feat(Test): Add configure test

---
+++ PCL.Test/ConfigureTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using PCL.Core.Helper.Configure;
+
+namespace PCL.Test
+{
+    [TestClass]
+    internal class ConfigureTest
+    {
+        [TestMethod]
+        public void TestJson()
+        {
+            var path = Path.Combine(Path.GetTempPath(), "PCLTest", $"{new Random().Next().ToString()}.json");
+
+            var j1 = new JsonConfigure(path);
+            j1.Set("awa",  "qwq");
+            Assert.IsTrue(j1.Get<string>("awa") == "qwq");
+
+            var j2 = new JsonConfigure(path);
+            Assert.IsTrue(j2.Get<string>("awa") == "qwq");
+            j2.Set("qwq", "awa");
+            Assert.IsTrue(j2.Get<string>("qwq") == "awa");
+        }
+    }
+}
