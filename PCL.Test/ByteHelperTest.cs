37b066e5c1008a64d4d45402d7311cf15de30362
tangge233 <tangge233@qq.com>
2025-06-23 20:33:05 +0800

    chore(byte): 加入一个小工具

---
+++ PCL.Test/ByteHelperTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PCL.Core.Helper;
+
+namespace PCL.Test;
+
+[TestClass]
+public class ByteHelperTest
+{
+    [TestMethod]
+    public void TestTransform()
+    {
+        var random = new Random(DateTime.Now.Millisecond);
+        var wathcer = new Stopwatch();
+        const int maxTestCount = 1000;
+        wathcer.Start();
+        for (int i = 0; i < maxTestCount; i++)
+        {
+            ByteHelper.GetReadableLength(random.Next(1024, 10240000));
+        }
+        wathcer.Stop();
+        Console.WriteLine($"Took {wathcer.ElapsedMilliseconds} ms to format {maxTestCount} data size.");
+        Console.WriteLine($"Avg: {(double)wathcer.ElapsedMilliseconds/maxTestCount} ms/per");
+    }
+}
