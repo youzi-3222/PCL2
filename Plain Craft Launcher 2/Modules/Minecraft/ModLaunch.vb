040811b67164784560f54ca56139173c45c27b29
youzi-3222 <youzi_2333@163.com>
2025-06-29 19:31:09 +0800

    fix(ModLaunch): 修复检查存档快捷启动失败的问题
    Closes PCL-Community/PCL2-CE#746

--- Plain Craft Launcher 2/Modules/Minecraft/ModLaunch.vb
+++ Plain Craft Launcher 2/Modules/Minecraft/ModLaunch.vb
@@ -1363,50 +1363,24 @@ LoginFinish:
 #Region "启动参数"
 
     Public Class LaunchArgument
-        Private _features As New Dictionary(Of String, String)
+        Private _features As New List(Of String)
         Public Sub New(Minecraft As McVersion)
             Dim curArgu As String = String.Empty
             If Minecraft.IsOldJson Then
-                '分隔开参数
-                Dim param = Minecraft.JsonObject("minecraftArguments").ToString.Split(" "c).ToList()
-                For Each p In param
-                    If p.StartsWithF("--") Then
-                        curArgu = p
-                        Continue For
-                    End If
-                    If p.StartsWithF("$") Then
-                        _features.Add(curArgu, p)
-                    End If
-                Next
+                _features = Minecraft.JsonObject("minecraftArguments").ToString.Split(" "c).ToList()
             Else
                 For Each item In Minecraft.JsonObject("arguments")("game")
                     If item.Type = JTokenType.String Then
-                        If item.ToString().StartsWithF("$") Then
-                            curArgu = item.ToString()
-                            Continue For
-                        End If
-                        If item.ToString().StartsWithF("--") Then
-                            _features.Add(curArgu, item.ToString())
-                        End If
+                        _features.Add(item.ToString)
                     ElseIf item.Type = JTokenType.Object Then
-                        For Each values In item("value")
-                            If values.ToString().StartsWithF("--") Then
-                                curArgu = values.ToString()
-                                _features.Add(curArgu, String.Empty)
-                            End If
-                            If values.ToString().StartsWithF("$") AndAlso
-                                _features.ContainsKey(curArgu) AndAlso
-                                String.IsNullOrEmpty(_features(curArgu)) Then
-                                _features(curArgu) = values.ToString()
-                            End If
-                        Next
+                        _features.AddRange(item("value").Select(Function(x) x.ToString))
                     End If
                 Next
             End If
         End Sub
 
         Public Function HasArguments(key As String)
-            Return _features.ContainsKey(key)
+            Return _features.Contains(key)
         End Function
     End Class
 
